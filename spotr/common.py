9676beee24ba0988cb5130f94edd7b9b12e0c056
vdth <vandenbrande52@hotmail.com>
2022-06-06 23:02:13 +0200

    added getTracksFromArtist

--- spotr/common.py
+++ spotr/common.py
@@ -104,12 +104,12 @@ def apiReqSpotify(urlExtension):
             waitForGivenTimeIns(0.5,1)
             logAction("msg - spotify.py - apiReqSpotify4 --> retrying request #" + str(retryCnt))
 
-            if retryCnt >= 10:
+            if retryCnt >= 30:
                 logAction("err - spotify.py - apiReqSpotify5 --> too many retries requesting acces token.")
 
                 '''--> save last result for debugging'''
                 with open (ROOT_DIR + "/logs/spotify_apiReqSpotify_LAST.json", 'w', encoding="utf-8") as fi:
-                    fi.write(str(response.json()))
+                    fi.write(json.dumps(response.json(), indent = 4))
                 return ''
 
             response = requests.get(url, headers=headers, verify=False)
@@ -181,7 +181,7 @@ def getNewAccessToken():
 
             '''--> save last result for debugging'''
             with open (ROOT_DIR + "/logs/spotify_getNewAccessToken_LAST.json", 'w', encoding="utf-8") as fi:
-                fi.write(str(response.json()))
+                fi.write(json.dumps(response.json(), indent = 4))
             return ''
     except Exception as ex:
         logAction("err - spotify.py - getNewAccessToken4.5 --> " + str(type(ex)) + " - " + str(ex.args) + " - " + str(ex))
@@ -191,7 +191,7 @@ def getNewAccessToken():
 
     '''--> save last result for debugging'''
     with open (ROOT_DIR + "/logs/spotify_getNewAccessToken_LAST.json", 'w', encoding="utf-8") as fi:
-       fi.write(str(response.json()))
+       fi.write(json.dumps(response.json(), indent = 4))
 
 
     '''--> check if (json-converted) response contains a new refresh_token.
@@ -288,7 +288,7 @@ def getTracksFromLikedList():
     
     '''--> check response before continuing'''
     if liked_tracks == '':
-        logAction("err - common.py - getTracksFromLikedList --> empty api response for liked tracks!")
+        logAction("err - common.py - getTracksFromLikedList2 --> empty api response for liked tracks!")
         return ''
 
 
@@ -306,13 +306,82 @@ def getTracksFromLikedList():
             offset = offset + limit
             if offset < total: #new request
                 liked_tracks = apiReqSpotify("me/tracks?offset=" + str(offset) + "&limit=" + str(limit))
+                if liked_tracks == '': #invalid api response
+                    logAction("err - common.py - getTracksFromLikedList3 --> empty api response for liked tracks!")
+                    return ''
                 continue
 
         return resultList
 
     except Exception as ex:
-        logAction("err - common.py - getTracksFromLikedList2 --> " + str(type(ex)) + " - " + str(ex.args) + " - " + str(ex))
+        logAction("err - common.py - getTracksFromLikedList4 --> " + str(type(ex)) + " - " + str(ex.args) + " - " + str(ex))
         logAction("TRACEBACK --> " + traceback.format_exc())
         return ''
+
+
+########################################################################################
+######################################### SPOTIFY ######################################
+def getTracksFromArtist(artistID, trackDetails):
+    '''--> Returns list with all track ID's or trackdetails for the given artist - pagination taken into account'''
+    '''In case of error, '' is returned'''
+    '''if trackDetails is TRUE, a list with dicts is returned containing detailed track info.  {id: trackid, artists: list with artists, title: title}'''
+    '''if FALSE, a list containing only the track ID's is returned'''
+
+
+    '''--> perform request, artist albums'''
+    logAction("msg - common.py - getTracksFromArtist --> requesting albums from artist " + artistID + ".") 
+    artist_albums = apiReqSpotify("artists/" + artistID + "/albums?offset=0&limit=50")
+
+
+    '''--> check response before continuing'''
+    if artist_albums == '':
+        logAction("err - common.py - getTracksFromArtist2 --> empty api response for artists albums!")
+        return ''
+
+
+    try:
+        '''--> check pagination - fill resultlist'''
+        resultList = []
+        total = artist_albums["total"]
+        limit = artist_albums["limit"]
+        offset = artist_albums["offset"]
+
+        while offset < total:
+            for album in artist_albums["items"]:
+                #album tracks
+                artist_tracks = apiReqSpotify("albums/" + album["id"] + "/tracks")
+                if artist_tracks != "": #valid api response
+                    for track in artist_tracks["items"]:
+                        if track["id"]: #check if valid item
+                            if trackDetails:
+                                #detailed track info
+                                #grab artist name(s)
+                                artist_list = []
+                                for artist in track["artists"]:
+                                    artist_list.append(artist["name"])
+                                artist_trck_dict_obj = {"id" : track["id"], "artists" : artist_list, "title" : track['name']}
+                                resultList.append(artist_trck_dict_obj)         
+                            else:
+                                #only track ID
+                                resultList.append(track["id"])
+                else: #invalid api response                  
+                    logAction("err - common.py - getTracksFromArtist3 --> empty api response for artist's album tracks (" + album["id"] + ").")
+                    return ''
+
+            offset = offset + limit
+            if offset < total: #new request
+                artist_tracks = apiReqSpotify("albums/" + album["id"] + "/tracks?offset=" + str(offset) + "&limit=" + str(limit))
+                if artist_tracks == '': #invalid api response
+                    logAction("err - common.py - getTracksFromArtist4 --> empty api response for artist's album tracks!")
+                    return ''
+                continue
+
+        return resultList
+
+    except Exception as ex:
+        logAction("err - common.py - getTracksFromArtist5 --> " + str(type(ex)) + " - " + str(ex.args) + " - " + str(ex))
+        logAction("TRACEBACK --> " + traceback.format_exc())
+        return ''
+
 
 ########################################################################################
